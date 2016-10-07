04a5c774f81de5ec8b9a59a010e56319e2dcfc25
jheftmann <jheftmann@mac.com>
2016-10-07 15:29:31 -0400

    first commit

---
+++ project-3/scripts.js
@@ -0,0 +1,18 @@
+// this line of code sets up your canvas
+var r = new Rune({
+  container: "body",
+  width: '100%',
+  height: '100%'
+});
+// write your own code below this line
+
+
+
+
+
+
+
+
+// and above this line
+// this line of code draws your shapes
+r.draw();
