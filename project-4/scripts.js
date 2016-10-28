ab4dbb164db3c540359711aadf9a31eae0402978
Patrice Clark-Peters <clarp811@newschool.edu>
2016-10-28 14:22:59 -0400

    newest one

---
+++ project-4/scripts.js
@@ -0,0 +1,85 @@
+// this is a comment
+$(document).ready(function(){
+
+
+
+
+	console.log("hello world!");
+var myGreeting = "hello, world with variables";
+console.log(myGreeting + "and also strings");
+var x = 100;
+var y = 5;
+
+console.log((x*y)+ "5");
+if (x > 5) {
+
+console.log("its more than 5");
+}else if (x == 100) {
+console. log("its 100");
+} else {
+	console.log("its neither 5 nor 100");
+}
+
+// prompt ("Fuck The World");
+
+//
+var answer = prompt ("want some drugs?")
+
+if (answer=="yes") {
+	$("body").prepend( answer + "want some drugs");
+} else {}
+
+if (answer == "yes") alert("copy bruh")
+if (answer == "no") alert("you wasting my time B")
+
+var answer2 = prompt("wait you a cop?");
+
+$("body").prepend( answer2 +"wait you a cop");
+
+if (answer == "yes") alert("*gunshots ring out*")
+if (answer == "no") alert("oh aigh")
+window.location = "https://www.youtube.com/watch?v=9-IZZs2Al5k"
+var answer3 = prompt("you wanna repeat that?");
+
+$("body").prepend( answer3 +"you wanna repeat that");
+
+if (answer3 == "yes") alert("*pulls knife on you*")
+if (answer3 == "no") alert("thats what I thought")
+
+var answer4 = prompt("is that so?");
+
+$("body").prepend( answer4 +"is that so");
+
+if (answer4 == "correct") alert("shanks you with a knife")
+if (answer4 == "nah") alert("ah aight")
+
+var answer5 = prompt("question, you know jacob?");
+
+$("body").prepend( answer5 +"question, you know jacob");
+
+if (answer5 == "yes") alert("cool i know jacob too")
+if (answer5 == "no") alert("guess you gotta get shot then")
+
+var answer6 = prompt("so what you feeling like buying?");
+
+$("body").prepend( answer5 +"so what you feeling like buying");
+
+if (answer6 == "crack") alert("tryna fuck your future up, thats 300")
+if (answer6 == "lean") alert("okay bootleg Future, thats 50 a bottle")
+if (answer6 == "weed") alert("420 blaze it")
+if (answer6 == "heroin") alert("listen kid, you're gonna be in a fucked up place")
+if (answer6 == "cocaine") alert("you aint no pablo escobar, nor el chapo")
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
